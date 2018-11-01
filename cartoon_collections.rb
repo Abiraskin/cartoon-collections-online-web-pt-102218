0c532dfdf6b5b49309f129a65938a53e1c1c2bd6
Abi Raskin <abiraskin@gmail.com>
2018-11-01 12:04:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,10 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  ingredients.find do |maybe_cheese|
+    cheese_types?(maybe_cheese)
+  end  
 end

3097c9443e9361c817fad310b74b03aa2eeb7e30
Abi Raskin <abiraskin@gmail.com>
2018-11-01 11:22:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |i|
-    planeteer_calls.capitalize.insert(1, "!")
-  end  
+    i.capitalize.insert(1, "!")
+  end
 end
 
 def long_planeteer_calls# code an argument here
